6c50b35bf21ace493f31271602659a641e685105
JohnCoene <jcoenep@gmail.com>
2023-11-07 08:48:04 +0100

    fix: load_all fn name

--- R/devtools-load-all.R
+++ R/devtools-load-all.R
@@ -6,7 +6,7 @@
 #' @param bundle_opts List of arguments passed to [bundle()].
 #'
 #' @export
-document <- \(..., bundle_opts = list()){
+load_all <- \(..., bundle_opts = list()){
   do.call(bundle, as.list(bundle_opts))
   devtools::load_all(...)
 }
