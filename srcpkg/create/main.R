7fb8c7636379aac5851e0e1a262f3bd484980baa
John Coene <jcoenep@gmail.com>
2023-12-30 10:57:19 +0100

    fix: create do not edit

--- srcpkg/create/main.R
+++ srcpkg/create/main.R
@@ -16,7 +16,7 @@ use_many <- \(src = "srcpkg"){
   # if src empty
   # if not it is likely that 
   # use_many was already run
-  if(lengthnst(list.files(src))) {
+  if(length(list.files(src))) {
     error(src, " already contains files\n")
     return(invisible())
   }
@@ -40,8 +40,8 @@ use_many <- \(src = "srcpkg"){
     jsonlite::write_json(".many", auto_unbox = TRUE, pretty = TRUE)
 
   # add DO_NOT_EDIT.R
-  fl <- system.file("templates/no-edit.R")
-  file.copy(fl, file.path("R", "00_DO_NOT_EDIT.R"), recursive = TRUE)
+  fl <- system.file("templates/no-edit.R", package = "many")
+  file.copy(fl, file.path("R", "00_DO_NOT_EDIT.R"))
 
   invisible(TRUE)
 }
